d3c3eb2901dbb503fe92f612cd7518b0e314594c
Shorif <shorifahamed571@gmail.com>
2023-11-07 02:21:27 +0600

    My schedules page created

--- src/Pages/MySchedules/MySchedules.jsx
+++ src/Pages/MySchedules/MySchedules.jsx
@@ -1,9 +1,15 @@
+import MyBookings from "./MyBookings";
+import Schedules from "./Schedules";
 
 
 const MySchedules = () => {
     return (
         <div>
-            this is my-schedules
+            <div className="flex justify-center items-center h-[200px] bg-red-500 mb-16">
+            <h2 className="text-3xl font-medium text-white">Schedules</h2>
+            </div>
+            <MyBookings></MyBookings>
+            <Schedules></Schedules>
         </div>
     );
 };
---
+++ src/Pages/MySchedules/Schedules.jsx
@@ -0,0 +1,12 @@
+
+const Schedules = () => {
+    return (
+        <div>
+            
+            <h2 className="text-2xl font-medium text-center">My Schedules</h2>
+        
+        </div>
+    );
+};
+
+export default Schedules;
